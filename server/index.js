96f9bdfecca74697eaebb4f7b6e9b7da24f13d35
sunyimin <sunyimin@duiba.com.cn>
2019-03-02 15:20:47 +0800

    返回静态页面

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const Koa = require('koa');
 const app = new Koa();
+const normal = require('./tpl/normal.js');
 app.use(async (ctx) => {
-   ctx.body = '网站首页'
+   ctx.type = 'text/html; charset=utf-8';
+   ctx.body = normal;
 })
 app.listen('5000');
